8435b79691dd3f958758f14fbc322b94f4768e98
ShuraKent <ghedc12@gmail.com>
2023-11-17 21:18:20 +0300

    add and test schedule, edit data again

--- src/my/Main.java
+++ src/my/Main.java
@@ -1,7 +1,47 @@
 package my;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+
+import my.schedule.Schedule;
+import my.schedule.data.Course;
+import my.schedule.data.Lesson;
+
 public class Main {
-    public static void main (String[] args){
-        System.out.println("AAAAA");
+    public static void main(String[] args) {
+        Schedule schedule = new Schedule();
+
+        Course mathCourse = new Course("Math");
+        Course physicsCourse = new Course("Physics");
+
+        Lesson mathLesson1 = new Lesson(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(10, 30), "101");
+        Lesson mathLesson2 = new Lesson(DayOfWeek.WEDNESDAY, LocalTime.of(9, 0), LocalTime.of(10, 30), "102");
+
+        Lesson physicsLesson1 = new Lesson(DayOfWeek.TUESDAY, LocalTime.of(13, 0), LocalTime.of(14, 30), "201");
+
+        mathCourse.addLesson(mathLesson1);
+        mathCourse.addLesson(mathLesson2);
+
+        physicsCourse.addLesson(physicsLesson1);
+
+        schedule.addCourse(mathCourse);
+        schedule.addCourse(physicsCourse);
+
+        schedule.addLesson(mathCourse, mathLesson1);
+        schedule.addLesson(mathCourse, mathLesson2);
+        schedule.addLesson(physicsCourse, physicsLesson1);
+
+        System.out.println("Math Course Schedule:");
+        List<Lesson> mathSchedule = schedule.getScheduleForCourse(mathCourse);
+        for (Lesson lesson : mathSchedule) {
+            System.out.println(lesson);
+        }
+
+        System.out.println("\nPhysics Course Schedule:");
+        List<Lesson> physicsSchedule = schedule.getScheduleForCourse(physicsCourse);
+        for (Lesson lesson : physicsSchedule) {
+            System.out.println(lesson);
+        }
     }
 }
--- src/my/schedule/Schedule.java
+++ src/my/schedule/Schedule.java
@@ -1,4 +1,38 @@
 package my.schedule;
 
+import my.schedule.data.Course;
+import my.schedule.data.Lesson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Schedule {
+    private Map<Course, List<Lesson>> scheduleMap;
+
+    public Schedule() {
+        this.scheduleMap = new HashMap<>();
+    }
+
+    public void addCourse(Course course) {
+        scheduleMap.put(course, new ArrayList<>());
+    }
+
+    public void addLesson(Course course, Lesson lesson) {
+        if (scheduleMap.containsKey(course)) {
+            List<Lesson> lessons = scheduleMap.get(course);
+            lessons.add(lesson);
+        }
+    }
+
+    public List<Lesson> getScheduleForCourse(Course course) {
+        return scheduleMap.getOrDefault(course, new ArrayList<>());
+    }
 }
+
+
+
+
+
+
